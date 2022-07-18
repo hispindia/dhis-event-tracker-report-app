f4050abec27b9165bd359b54b6848dd3acee4efc
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2022-07-18 11:44:36 +0530

    create tracker-event-report default-v236

--- scripts/leftbar-menu-controller.js
+++ scripts/leftbar-menu-controller.js
@@ -5,10 +5,9 @@ msfReportsApp.controller('LeftBarMenuController',
         $scope.showTodaySchedule = function () {
             $location.path('/schedule-today').search();
         };
-        // $scope.showEventReport = function(){
-        //     $location.path('/event-report').search();
-        // };
-
+        $scope.showEventReport = function(){
+            $location.path('/event-report').search();
+        };
         $scope.showTrackerReport = function () {
             $location.path('/tracker-report').search();
         };
