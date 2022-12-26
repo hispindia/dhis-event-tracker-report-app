7b38378cb9e1f31595311cbab4be34984ced541b
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2022-12-26 12:48:20 +0530

    add link for standard report

--- scripts/leftbar-menu-controller.js
+++ scripts/leftbar-menu-controller.js
@@ -1,7 +1,8 @@
 //Controller for column show/hide
 msfReportsApp.controller('LeftBarMenuController',
     function ($scope,
-        $location) {
+        $location,
+        $window) {
         $scope.showTodaySchedule = function () {
             $location.path('/schedule-today').search();
         };
@@ -12,5 +13,8 @@ msfReportsApp.controller('LeftBarMenuController',
 
         $scope.showTrackerReport = function () {
             $location.path('/tracker-report').search();
+        };
+        $scope.showPatientReferrelReport = function(){
+            $window.open('http://172.105.47.158/hp/dhis-web-reporting/getReportParams.action?uid=vBVVuET6zwY&mode=report&type=HTML', '_self')
         };
     });
